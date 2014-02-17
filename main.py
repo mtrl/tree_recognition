f268f3c27921480790f098b5e46c43e9c89b431a
Mark Williams <m.williams@contentbloom.com>
2014-02-17 09:10:25 +0000

    Script IDentifies tree templates in maps

--- main.py
+++ main.py
@@ -2,38 +2,30 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('images/crop.jpg',0)
-img2 = img.copy()
-template = cv2.imread('images/tree.jpg',0)
-w, h = template.shape[::-1]
+maps = ['crop2.jpg', 'crop.jpg']
 
-# All the 6 methods for comparison in a list
-#methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-#            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+f = open('coords.txt', 'w+')
 
-methods = ['cv2.TM_CCOEFF']
+for map in maps:
+	f.write("----------------\r\nMap: " + map + "\r\n----------------\r\n");
+	img_rgb = cv2.imread('images/' + map)
+	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+	template = cv2.imread('images/tree.jpg',0)
+	w, h = template.shape[::-1]
 
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
+	res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+#	res = cv2.matchTemplate(img_gray,template,cv2.TM_SQDIFF)
 
-    # Apply template Matching
-    res = cv2.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-
-    cv2.rectangle(img,top_left, bottom_right, 0, 2)
-
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-
-    plt.show()
+	threshold = 0.35
+	loc = np.where( res >= threshold)
+	tree_count = 0
+	for pt in zip(*loc[::-1]):
+		tree_count += 1
+		cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+		f.write("X: " + str(pt[0]) + " Y: " + str(pt[1]) + "\r\n")
+		
+	print tree_count
+	cv2.imwrite('res' + map,img_rgb)
+	print "Done " + map
+	
+f.close()
